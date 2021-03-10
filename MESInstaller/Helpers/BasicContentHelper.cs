1c473c78cf57f106287427b9a75ae683b3e34618
Luu Van Sy <sy200498@gmail.com>
2021-03-10 09:02:01 +0700

    1. Add basic content copy functions

--- MESInstaller/Helpers/BasicContentHelper.cs
+++ MESInstaller/Helpers/BasicContentHelper.cs
@@ -1,10 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
+using System.IO.Compression;
+using System.Windows;
 
 namespace MESInstaller.Helpers
 {
     public class BasicContentHelper
     {
+        // D:\MESContent
+        //              |---TMAX.zip
+        //              |---AGENT.zip
+        //              |---MES.zip
+        public static string TMAX_TargetDirectory = @"C:\TMAX";
+        public static string AGENT_TargetDirectory = @"D:\Agent";
+        public static string MES_TargetDirectory = @"D:\MES";
+
+        public static void ContentCopy(string Content_SourceDirectory)
+        {
+            CopyAGENT(Content_SourceDirectory);
+            CopyMES(Content_SourceDirectory);
+            CopyTMAX(Content_SourceDirectory);
+            MessageBox.Show("Copy and Extract done successfully.");
+        }
+
+        public static void CopyMES(string Content_SourceDirectory)
+        {
+            // Extract MES content to MES_Directory
+            ZipFile.ExtractToDirectory(Path.Combine(Content_SourceDirectory, "MES.zip"), MES_TargetDirectory);
+        }
+        public static void CopyAGENT(string Content_SourceDirectory)
+        {
+            // Extract AGENT content to AGENT_Directory
+            ZipFile.ExtractToDirectory(Path.Combine(Content_SourceDirectory, "AGENT.zip"), AGENT_TargetDirectory);
+        }
+        public static void CopyTMAX(string Content_SourceDirectory)
+        {
+            //Extract TMAX content to  TMAX_Directory
+            ZipFile.ExtractToDirectory(Path.Combine(Content_SourceDirectory, "TMAX.zip"), TMAX_TargetDirectory);
+        }
     }
 }
