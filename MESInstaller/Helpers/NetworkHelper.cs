5f5553f0a1889ad5c0803337ec5c4542d68463a9
DungNH <huudung@topengnet.com>
2021-03-10 08:57:45 +0700

    1. Design MVVM framework 2. Design basic UI

--- MESInstaller/Helpers/NetworkHelper.cs
+++ MESInstaller/Helpers/NetworkHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace MESInstaller.Helpers
---
+++ MESInstaller/Models/Define.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MESInstaller.Models
+{
+    public class Define
+    {
+        public static readonly List<string> LINE_LIST = new List<string>
+        {
+            "IR_LINE",
+            "VCM_LINE",
+        };
+
+        public static readonly List<string> VCM_MACHINE_LIST = new List<string>
+        {
+            "V200_CarrierLoading",
+            "V200_SolderPaste",
+            "V200_WeldingBossPin",
+            "V200_Jetting441",
+            "V300_USPCutting",
+            "V500_CarrierBaseAssemble",
+            "V300_FullAssyAssemble",
+            "V300_USPBonding",
+            "V300_FullBonding",
+            "V700_FunctionInspection",
+        };
+
+        // Todo: Modify this list
+        public static readonly List<string> IR_MACHINE_LIST = new List<string>
+        {
+            "LensAssemble_OLD",
+            "LensAssemble_NEW",
+            "Dispensing_VeryOLD",
+            "Dispensing_OLD",
+            "Dispensing_NEW",
+        };
+    }
+}
